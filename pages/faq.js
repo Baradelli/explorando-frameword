e9f84f60a8f6e7ec353648d62abd0931f8f91934
Vitor Baradelli <94546015+Baradelli@users.noreply.github.com>
2023-10-13 16:58:18 +0000

    :+1:

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,23 @@
+import Head from "next/head";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
-export async function getStaticProps() {
+/**
+ * SST - Static Site Generation
+ * SSR - Server Side Rendering
+ * ISC - Incremental Static Generation
+ * 
+ * getStaticProps()
+ *  - Os dados serão carregado apenas no momento de geração do build, com o build gerado ele não vai mais cair nessa função.
+ * 
+ * getServerSideProps()
+ *  - Vai funcionar basicamente como um useEffect, rodando a cada acesso que você recebe.
+ * 
+ * modo DEV
+ *  - No modo dev os 2 vão rodar a cada acesso que recebe.
+ * 
+ */
+
+export async function getServerSideProps() {
   const FAQ_API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
 
@@ -24,22 +40,28 @@ export default function FAQPage({ faq }) {
   // useEffect(() => {}, []);
 
   return (
-    <div>
-      <h1>Alura Caess - Página de perguntas</h1>
-      <Link href="/" passHref>
-        Ir para HOME
-      </Link>
+    <>
+      <Head>
+        <title>FAQ - Alura Cases Campanha</title>
+      </Head>
 
-      <ul>
-        {faq.map(({ answer, question }) => (
-          <li key={question}>
-            <article>
-              <h2>{question}</h2>
-              <p>{answer}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <div>
+        <h1>Alura Caess - Página de perguntas</h1>
+        <Link href="/" passHref>
+          Ir para HOME
+        </Link>
+
+        <ul>
+          {faq.map(({ answer, question }) => (
+            <li key={question}>
+              <article>
+                <h2>{question}</h2>
+                <p>{answer}</p>
+              </article>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
